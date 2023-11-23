0db75b313f820684ba6e7dd4bdd91d2d1c8593a2
david veizaga <qqqcruzito@gmail.com>
2023-11-23 04:58:32 -0400

    añadi

--- src/Profesores/Prueba.java
+++ src/Profesores/Prueba.java
@@ -4,6 +4,8 @@ public class Prueba {
     public static void main(String[] args) {
         Profesor profesor1 = new ProfesorTitular();
         Profesor profesor2 = (Profesor)profesor1;
+        Profesor profesor3 = new Profesor();
+        profesor3.imprimir();
         profesor2.imprimir();
         profesor1.imprimir();
     }
--- src/ejercicio3_2/main.java
+++ src/ejercicio3_2/main.java
@@ -4,10 +4,11 @@ import java.util.Locale;
 
 public class main {
     public static void main(String[] args) {
-
         Cadena cadena = new Cadena();
+
 // Determina la longitud de la cadena
         int longitud = cadena.cadenaInicial.length();
+
         System.out.println("La longitud del String es = " + longitud);
 // Elimina espacios en blanco al comienzo y al final
         String cadenaSinEspacios = cadena.cadenaInicial.trim();
@@ -29,8 +30,43 @@ public class main {
         System.out.println("Los String son iguales = " + comparación);
         //Un método que reciba como parámetro un String y calcule
         //cuántas mayúsculas tiene.
-        String mayuscula= String.valueOf(cadena.equals("Programación Orientadaa Objeto"));
-        System.out.println(" se repiten: "+mayuscula);
+        String mayuscula = String.valueOf(cadena.equals("Programación Orientadaa Objeto"));
+
+
+        //textoinvertido
+        String textoInvertido = "";
+        for (int i = cadena.cadenaInicial.length() - 1; i >= 0; i--) {
+            textoInvertido = textoInvertido + cadena.cadenaInicial.charAt(i);
+        }
+        System.out.println("Texto invertido: "+textoInvertido);
+
+        // contarMayusculasMinusculas
+        int mayusculas = 0;
+        int minusculas = 0;
+        for (int i = 0; i < cadena.cadenaInicial.length(); i++) {
+            char c = cadena.cadenaInicial.charAt(i);
+            if (Character.isUpperCase(c)) {
+                mayusculas++;
+            } else if (Character.isLowerCase(c)) {
+                minusculas++;
+            }
+        }
+
+        System.out.println("Mayúsculas: " + mayusculas);
+        System.out.println("Minúsculas: " + minusculas);
+
+        String TextoSinEspacios = cadena.cadenaInicial.replaceAll(" ", "");
+        System.out.println("Texto sin espacios: " + TextoSinEspacios);
+
+        char letra = 'b';
+        int cont = 0;
+        for (int i = 0; i < cadena.cadenaInicial.length(); i++) {
+            char c = cadena.cadenaInicial.charAt(i);
+            if (c == letra || Character.toLowerCase(c) == letra || Character.toUpperCase(c) == letra) {
+                cont++;
+            }
+        }
+        System.out.println("La letra '" + letra + "' aparece " + cont + " veces.");
 
     }
-  }
+}
